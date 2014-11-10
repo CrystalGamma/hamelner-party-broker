07e6bb103448f1b06acf54cee63104f6db27dd07
anwender <westhof.dev@gmail.com>
2014-11-10 15:02:49 +0100

    Kunde mit methoden gefüllt

--- src/Kunde.java
+++ src/Kunde.java
@@ -2,4 +2,88 @@
  * Created by jona on 10.11.14.
  */
 public class Kunde {
+	String name, vorName, straﬂe, ort;
+	int iD, hausnummer, plz, umsatz;
+	public void setName()
+	{
+		
+	}
+	public void setVorName()
+	{
+		
+	}
+	public void setStraﬂe()
+	{
+		
+	}
+	public void setHausnummer()
+	{
+		
+	}
+	public void setPlz()
+	{
+		
+	}
+	public void setOrt()
+	{
+		
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String getVorName()
+	{
+		return vorName;
+	}
+	public int getID()
+	{
+		return iD;
+	}
+	public String getStraﬂe()
+	{
+		return straﬂe;
+	}
+	public int getHausnummer()
+	{
+		return hausnummer;
+	}
+	public int getPlz()
+	{
+		return plz;
+	}
+	public String getOrt()
+	{
+		return ort;
+	}
+	public void rueckgabe(LagerPosten LagerPosten, int zeit, int menge)
+	{
+		
+	}
+	public void verlustmelden(LagerPosten LagerPosten, int zeit, int menge)
+	{
+		
+	}
+	public void ausleihe(Ausleihe Ausleihe)
+	{
+		
+	}
+	public void abrechnung()
+	{
+		
+	}
+	public int berechneUmsatz()
+	{
+		return umsatz;
+	}
+	public String getTransaktionen()
+	{
+		return null;
+	}
+	public void kaufen(Artikel Artikel, int menge)
+	{
+		
+	}
+	
+	
 }
