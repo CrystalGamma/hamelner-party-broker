de1e3e724999c594e884bec05eaa6697c608592f
Florian Bussmann <kontakt@florianbussmann.de>
2014-11-10 16:34:08 +0100

    Berechnung in RechnungsPosten angepasst

--- src/Verlust.java
+++ src/Verlust.java
@@ -1,10 +1,17 @@
 public class Verlust extends RechungsPosten {
-	public Verlust() {
-
+	public Verlust(Ausleihe pAusleihe, int pMenge, int zeitDelta) {
+		this.menge = pMenge;
+		this.artikel = pAusleihe.getPosten();
+		
+		long startZeit = pAusleihe.getStartZeit();
+		long endZeit = pAusleihe.getEndZeit();
+		long zeitDelta = endZeit-startZeit;
+		this.betrag = ((LagerPosten) this.artikel).verlustGebuehr(0, pMenge, zeitDelta);
 	}
-	
+
 	@Override
 	public String toString() {
-		return "";
+		return "Rechnungspunkt für den Verlust von " + this.menge + "x"
+				+ this.artikel + " in Höhe von " + this.betrag + "€.";
 	}
 }
