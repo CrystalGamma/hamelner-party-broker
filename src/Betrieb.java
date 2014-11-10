3faf7c6a1b3c71eafa68809027d1394470e835ff
Florian Bussmann <kontakt@florianbussmann.de>
2014-11-10 15:02:52 +0100

    Stub für Klasse Betrieb

--- src/Betrieb.java
+++ src/Betrieb.java
@@ -1,5 +1,58 @@
-/**
- * Created by jona on 10.11.14.
- */
 public class Betrieb {
+	private long zeit;
+	private Kunde aktuellerKunde;
+	private Kunde[] kunden;
+	private Artikel[] artikel;
+
+	public Betrieb() {
+
+	}
+
+	private String abrechung(int pKundenID) {
+		return "";
+	}
+	
+	private void anDerUhrDrehen(int pZeit){
+		zeit += pZeit;
+	}
+
+	private String bestandAuflisten(int pModus) {
+		return "";
+	}
+
+	private void datenAendern(int pKundenID) {
+
+	}
+	
+	private void kundeWechseln(){
+		
+	}
+
+	private void rueckgabe() {
+
+	}
+	
+	private String transaktionen(int pKundenID){
+		return "";
+	}
+
+	private String umsatzBericht() {
+		return "";
+	}
+
+	private void verkaufen() {
+
+	}
+
+	private void verleih() {
+
+	}
+
+	private void verlust() {
+
+	}
+
+	public static void main(String[] args) {
+		new Betrieb();
+	}
 }
