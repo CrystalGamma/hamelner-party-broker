25d36f0802f5d75f2ccfe253fd4ef1c16e04424c
Florian Bussmann <kontakt@florianbussmann.de>
2014-11-10 16:18:00 +0100

    Neue Methode istVerfuegbar in Artikel

--- src/Artikel.java
+++ src/Artikel.java
@@ -1,9 +1,10 @@
 public abstract class Artikel {
 	protected String name;
-	protected double handlingPauschale;
+	protected int handlingPauschale;
 	
 	public abstract String toString();
 	public abstract boolean istVerkaeuflich();
 	public abstract boolean istVerleihbar();
+	public abstract boolean istVerfuegbar();
 	public abstract int kaufPreis(int menge);
 }
