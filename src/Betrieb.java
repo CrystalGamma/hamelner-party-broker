f68cd6d8d75cc3966847e5335c1c18a91c96aeaf
anwender <westhof.dev@gmail.com>
2014-11-27 10:18:36 +0100

    hashmap

--- src/Betrieb.java
+++ src/Betrieb.java
@@ -1,8 +1,9 @@
+import java.util.HashMap;
 public class Betrieb {
 	private long zeit;
 	private Kunde aktuellerKunde;
-	private Kunde[] kunden;
 	private Artikel[] artikel;
+	private HashMap<Integer, Kunde> kunden;
 
 	public Betrieb() {
 		artikel = new Artikel[] {
@@ -10,7 +11,19 @@ public class Betrieb {
 			new LagerPosten("Test2", 200, true, true, 300),
 			new LagerPosten("Test3", 10000, false, true, 50),
 			new LagerPosten("Test4", 3000, true, true, 42),
-			new LagerPosten("Test5", 159900, false, false, 3)
+			new LagerPosten("Test5", 159900, false, false, 3),
+			/*LagerPosten(String name,int verkaufspreis, int leihGebuehr, int handlingPauschale,
+			int verlustGebuehr, boolean verkaeuflich, boolean verleihbar,
+			int bestand)*/
+			new LagerPosten("Bierglas", 160,80,20,100, true, true, 1250),
+			new LagerPosten("Pinneken", 140,100,20,50, true, true, 1490),
+			new LagerPosten("Dose Lauwarme Cervisia", 200,0,0,0, true, false, 520),
+			new LagerPosten("Faß Lauwarme Cervisia", 5000,0,0,0, true, false, 10),
+			new LagerPosten("kleine Zapfanlage",20900,8000,900,9000, true,true,18),
+			new LagerPosten("große Zapfanlage",0,22000,2900,80000, false, true, 3),
+			new LagerPosten("10er Bierdeckel",15,0,0,0, true, false, 3900),
+			new LagerPosten("Toilettenwagen",0,8900,29900,0, false, true, 6),
+			new DienstLeistung("Frischwasserinstallation",18500)
 		};
 	}
 
@@ -40,6 +53,7 @@ public class Betrieb {
 	}
 
 	private void datenAendern(int pKundenID) {
+		
 	}
 	
 	private void kundeWechseln(Kunde kunde){
