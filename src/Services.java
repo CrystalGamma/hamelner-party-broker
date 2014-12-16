ee5f2b57bce62f8fc56e1d0729568daeb83601aa
Jona Stubbe (CrystalGamma) <dev@jona-stubbe.de>
2014-12-16 12:52:29 +0100

    eurozeichen in geldString

--- src/Services.java
+++ src/Services.java
@@ -1,5 +1,5 @@
 public class Services {
     public static String geldString(int cents) {
-        return  String.format("%.2f", (float)cents/100);
+        return  String.format("%.2f€", (float)cents/100);
     }
 }
